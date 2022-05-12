368c596e163035e596969cbe68b5fb30e53849af
Robson Farias <robson.lucas@verumoms.com.br>
2022-05-12 20:30:18 -0300

    move up

---
+++ ActionAndActionChaining/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ActionAndActionChaining
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Action action = () => MyFunction("Eu sou um parametro");
+            Action actionTwo = MyFunction;
+
+            action();
+            actionTwo();
+
+            Action handle;
+
+            handle = FirstName;
+            handle += SecondName;
+            handle += LastName;
+
+            handle();
+        }
+
+        static void MyFunction(string text)
+            => Console.WriteLine(text);
+
+        static void MyFunction()
+            => Console.WriteLine("Sou uma sobrecarga");
+
+
+        static void FirstName() 
+            => Console.WriteLine("Robson");
+
+        static void SecondName() 
+            => Console.WriteLine("Lucas");
+
+        static void LastName()
+            => Console.WriteLine("Farias");
+
+
+    }
+}
